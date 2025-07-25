b8c36f4d54130f48127a011b8d2fa90d359dbeeb
imulchrone <ianmulchrone@gmail.com>
2025-07-25 15:16:19 -0500

    Add files via upload

--- scoreboard.py
+++ scoreboard.py
@@ -42,12 +42,12 @@ def build_team_table(team_name, rows):
     def build_header():
         return dbc.Row([
             dbc.Col("", width=1, style=centered()),
-            dbc.Col("Member", width=2, style={**centered(font_size='2.8vw')}),
-            dbc.Col("Players", width=4, style=centered(font_size='2.8vw')),
+            dbc.Col("Member", width=2, style={**centered(font_size='2.8vw', justifyContent='right'),'paddingRight':'4px'}),
+            dbc.Col("Players", width=4, style={**centered(font_size='2.8vw', justifyContent='left'),'paddingLeft':'38px'}),
             dbc.Col("G1", width=1, style={**centered(font_size='2.8vw')}),
             dbc.Col("G2", width=1, style={**centered(font_size='2.8vw', justifyContent='center'),'paddingLeft':'4px'}),
             dbc.Col("G3", width=1, style={**centered(font_size='2.8vw', justifyContent='left'),'paddingLeft':'1px'}),
-            dbc.Col("Total", width=1, style={**centered(font_size='2.3vw'),'paddingLeft':'10px'}),
+            dbc.Col("Total", width=1, style={**centered(font_size='2.5vw'),'paddingLeft':'10px'}),
         ], style={
             'color': 'white',
             'fontWeight': 'bold',
@@ -73,10 +73,10 @@ def build_team_table(team_name, rows):
                     style={'display': 'flex', 'alignItems': 'center', 'height': '100%'}
                 ), width=4
             ),
-            dbc.Col(str(row['g1']), width=1, style={**centered(font_size='2.8vw')}),
-            dbc.Col(str(row['g2']), width=1, style={**centered(font_size='2.8vw')}),
-            dbc.Col(str(row['g3']), width=1, style={**centered(font_size='2.8vw')}),
-            dbc.Col(str(row['total']), width=1, style={**centered(font_size='2.8vw'), 'paddingRight':'2px'}),
+            dbc.Col(str(row['g1']), width=1, style={**centered(font_size='3.0vw')}),
+            dbc.Col(str(row['g2']), width=1, style={**centered(font_size='3.0vw')}),
+            dbc.Col(str(row['g3']), width=1, style={**centered(font_size='3.0vw')}),
+            dbc.Col(str(row['total']), width=1, style={**centered(font_size='3.0vw'), 'paddingRight':'2px'}),
         ], style={
             'fontFamily': 'monospace',
             'color': 'white',
